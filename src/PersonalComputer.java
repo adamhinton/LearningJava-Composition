77a1736f9b278ed1d190b780524e17cd7273e218
adamd <adam.d.hinton@gmail.com>
2024-02-24 11:41:20 -0500

    Flesh out Getters

--- src/PersonalComputer.java
+++ src/PersonalComputer.java
@@ -13,6 +13,15 @@ public class PersonalComputer extends Product {
         this.motherboard = motherboard;
     }
 
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
 
+    public Monitor getMonitor() {
+        return monitor;
+    }
 
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
 }
